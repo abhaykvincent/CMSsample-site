1fc2dc37b5ede804507a2858abb54fde077d38fe
Abhay Vincent <abhaykvincent@gmail.com>
2020-01-28 14:35:38 -0500

    yghhjk

--- themes/sample-theme/footer.php
+++ themes/sample-theme/footer.php
@@ -12,7 +12,7 @@
 ?>
 
 	</div><!-- #content -->
-<!-- dksjcnbvhcn -->
+<!-- hfgdcfbvhfdcnb -->
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sample-theme' ) ); ?>">
